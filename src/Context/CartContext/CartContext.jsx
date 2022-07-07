2727d4bd29375acd02cb321fe0198cee5476b0b5
Lautaro Chaar <lautarochaar@hotmail.com>
2022-07-07 18:14:15 -0300

    CartContext

--- src/Context/CartContext/CartContext.jsx
+++ src/Context/CartContext/CartContext.jsx
@@ -1,48 +1,57 @@
-import React from 'react';
-import { createContext, useState } from 'react';
-
-export const cartContext = createContext(null);
-
+import React from "react";
+ 
+export const cartContext = React.createContext(null);
+ 
+const defaultCart = [];
+ 
 export default function CartContext({ children }) {
-
-	const [cart, setCart] = useState([]);
-	
-	const addItem = (productDetail, quantity ) => {
-		const addedProduct = {
-			id: productDetail.id, 
-			title: productDetail.title, 
-			category: productDetail.category,   
-			stock: productDetail.stock, 
-			initial: productDetail.initial,
-			quantity: quantity,	
-			price: productDetail.price
-		}
-		
-		setCart(cart);
-		console.log(cart)
-		// alert(`Agregaste ${initial} producto/s al carrito de compra.`)
-	};
-
-//   const addItem = (item, quantity) => {
-
-// 	}
-
-// 	const removeItem = (itemId) => {
-
-// 	}
-
-	// const clear = () => {
-	// 	setCart([]);
-	// }
-// 	const isInCart = (id) => {
-
-// 		return i | -1;
-// 	}
-
-
+  
+  const [cart, setCart] = React.useState(defaultCart);
+ 
+  const isInCart = (productId) =>
+    cart.some((product) => product.id === productId);
+ 
+  const updateProductQuantity = (product, quantity) =>
+    setCart((prevCart) =>
+      prevCart.map((item) =>
+        item.id === product.id
+          ? {
+              ...item,
+              quantity: item.quantity + quantity,
+            }
+          : item
+      )
+    );
+ 
+  const addNewProduct = (product, quantity) =>
+    setCart((prevCart) => [...prevCart, { ...product, quantity }]);
+ 
+  const addItem = (product, quantity) => {
+    if (isInCart(product.id)) {
+      updateProductQuantity(product, quantity);
+    } else {
+      addNewProduct(product, quantity);
+    }
+  };
+ 
+  const removeProduct = (product) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== product.id));
+  };
+ 
+  const clear = () => setCart(defaultCart);
+ 
+  const getQuantity = (productId) => {
+    const item = cart.find((product) => product.id === productId);
+    return item ? item.quantity : 0;
+  };
+ 
+  console.info("cart: ", cart);
+ 
   return (
     <>
-    	<cartContext.Provider value={{ addItem }}>{children}</cartContext.Provider>
+      <cartContext.Provider value={{ addItem, getQuantity }}>
+        {children}
+      </cartContext.Provider>
     </>
-  )
+  );
 }
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,59 +1,69 @@
-import * as React from 'react';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import { Link } from 'react-router-dom';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import './ItemCount.css';
-import { cartContext } from '../../Context/CartContext/CartContext';
-import { useContext } from 'react';
-
-
+import * as React from "react";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { Link } from "react-router-dom";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import "./ItemCount.css";
+import { cartContext } from "../../Context/CartContext/CartContext";
+import { useContext } from "react";
+ 
 export default function ItemCount({ stock, initial, onAdd, productDetail }) {
-
   const [count, setCount] = React.useState(initial);
-  const { addItem } = useContext(cartContext);
-  
+  const { addItem, getQuantity } = useContext(cartContext);
+ 
+  const prevAddedQuantity = getQuantity(productDetail.id);
+        
   return (
-   
-      <div>
-        <ButtonGroup className='btnGroup'>
-          <div className='addCartbtn'>
-              <Button
-              variant='text'
-              aria-label="reduce"
-              onClick={() => {
-                setCount(Math.max(count - 1, 0));
-              }}
-            >
-              <RemoveIcon fontSize="small" sx={{color: '#66467f'}} />
-            </Button>
-            <p className='numberOfProducts'>{count}</p>
-            <Button
-              variant='text'
-              aria-label="increase"
-              onClick={() => {
-                if (count < stock) {
-                  setCount(count + 1);
-                }
-              }}
-            >
-              <AddIcon fontSize="small" sx={{color: '#66467f'}} />
-            </Button>
-            <Button
-            onClick={()=>{
-              if ((count <= stock) && (count !== 0)) {
+    <div>
+      <ButtonGroup className="btnGroup">
+        <div className="addCartbtn">
+          <Button
+            variant="text"
+            aria-label="reduce"
+            onClick={() => {
+              setCount(Math.max(count - 1, 0));
+            }}
+          >
+            <RemoveIcon fontSize="small" sx={{ color: "#66467f" }} />
+          </Button>
+          <p className="numberOfProducts">{count}</p>
+          <Button
+            variant="text"
+            aria-label="increase"
+            onClick={() => {
+              if (prevAddedQuantity + count < stock) {
+                setCount(count + 1);
+              }
+            }}
+          >
+            <AddIcon fontSize="small" sx={{ color: "#66467f" }} />
+          </Button>
+          <Button
+            onClick={() => {
+              const totalQuantity = prevAddedQuantity + count;
+              if (totalQuantity <= stock && count !== 0) {
                 initial += count;
                 onAdd(initial);
+                addItem(productDetail, count);
+                setCount(0);
               }
-            } }
-            variant='text'>
-              <AddShoppingCartIcon fontSize="small" sx={{color: '#66467f'}}></AddShoppingCartIcon>
-            </Button>
-          </div>
-          <Button variant='text'><Link className='buyBtn' to={'/cart'} >Comprar</Link></Button>
-        </ButtonGroup>
-      </div>
+            }}
+            variant="text"
+          >
+            <AddShoppingCartIcon
+              fontSize="small"
+              sx={{ color: "#66467f" }}
+            ></AddShoppingCartIcon>
+          </Button>
+        </div>
+        <Button variant="text">
+          <Link className="buyBtn" to={"/cart"}>
+            Comprar
+          </Link>
+        </Button>
+      </ButtonGroup>
+    </div>
   );
 }
